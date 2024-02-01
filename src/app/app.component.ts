7f3aeeb51997c9e01bbc5e3cfd09f27dd22a6d8d
Marcos Lopez <markosstone@gmail.com>
2024-02-02 00:46:38 +0100

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './head/head.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FootComponent } from './foot/foot.component';
+import { MarketComponent } from './market/market.component';
+import { MyWalletComponent } from './my-wallet/my-wallet.component';
+import { Page404Component } from './page404/page404.component';
+import { CreditsComponent } from './credits/credits.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeadComponent,NavMenuComponent,FootComponent,MarketComponent,MyWalletComponent,Page404Component,CreditsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
